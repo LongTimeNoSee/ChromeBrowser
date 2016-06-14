e0d2409e4a82e87ee1796f401203221cf493f8ff
wangbin <wangbin@2345.com>
2016-06-14 16:55:02 +0800

    repo project

---
+++ ChromeBrowser/app/src/main/java/browser/chrome/com/chromebrowser/SplashActivity.java
@@ -0,0 +1,14 @@
+package browser.chrome.com.chromebrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wangbin on 2016/6/14.
+ */
+public class SplashActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
